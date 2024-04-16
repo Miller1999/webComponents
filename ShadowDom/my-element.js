1bcdeb9fe4a22ed752b86179b14ac2bbe61b4842
Miller Arias <millerjavierarias@gmail.com>
2024-04-15 23:24:10 -0500

    ShadowDOM y slot

---
+++ ShadowDom/my-element.js
@@ -0,0 +1,34 @@
+class myElement extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: "open" });
+	}
+	getStyles() {
+		return `
+			<style>
+				h2 {
+					color:red
+				}
+			</style>
+		`;
+	}
+	getTemplate() {
+		const template = document.createElement("template");
+		// Se agrega slot para poder ingresar informacion desde afuera del componente
+		template.innerHTML = `
+			<section>
+				<h2><slot></slot></h2>
+			</section>
+			${this.getStyles()}
+		`;
+		return template;
+	}
+	render() {
+		this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+	}
+	connectedCallback() {
+		this.render();
+	}
+}
+
+customElements.define("my-element", myElement);
--- slot/my-element.js
+++ slot/my-element.js
@@ -2,23 +2,36 @@
 class myElement extends HTMLElement {
 	constructor() {
 		super();
+		//Inicializamos el Shadow DOM
+		this.attachShadow({ mode: "open" });
+	}
+	getStyles() {
+		return `
+			<style>
+				h2 {
+					color:red
+				}
+			</style>
+		`;
 	}
 	//Se genera este metodo para usar el template
 	getTemplate() {
 		const template = document.createElement("template");
 		template.innerHTML = `
 			<section>
-				<h2>Hola mundo template</h2>
+				<h2>Hola mundo dentro del shadowDOM</h2>
 				<div>
-					<p>Texto del template</p>
+					<p>Texto del shadowDOM</p>
 				</div>
 			</section>
+			${this.getStyles()}
 		`;
 		return template;
 	}
 	// Por buenas practicas se deben separar responsabilidades
 	render() {
-		this.appendChild(this.getTemplate().content.cloneNode(true));
+		// Ahora para renderizar se debe utilizar de la siguiente manera
+		this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
 	}
 	connectedCallback() {
 		this.render();
