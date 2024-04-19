ea954ddcbe71a9caafb66ab6553ce6f5d269d345
Miller Arias <millerjavierarias@gmail.com>
2024-04-19 16:50:31 -0500

    Attributes

---
+++ atributos/my-element.js
@@ -0,0 +1,42 @@
+class myElement extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: "open" });
+		this.title = this.getAttribute("title");
+		this.parrafo = this.getAttribute("parrafo");
+		this.img = this.getAttribute("img");
+	}
+	getStyles() {
+		return `
+			<style>
+				h2 {
+					color:red
+				}
+			</style>
+		`;
+	}
+	getTemplate() {
+		const template = document.createElement("template");
+		// Se agrega slot para poder ingresar informacion desde afuera del componente
+		// Con el atributo name se puede renderizar mas de un slot
+		template.innerHTML = `
+			<section>
+				<h2>${this.title}</h2>
+				<div>
+					<p>${this.parrafo}</p>
+					<img src=${this.img}/>
+				</div>
+			</section>
+			${this.getStyles()}
+		`;
+		return template;
+	}
+	render() {
+		this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+	}
+	connectedCallback() {
+		this.render();
+	}
+}
+
+customElements.define("my-element", myElement);
